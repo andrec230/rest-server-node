7daab36723eb86a92b1540d8be361ef9d800ae8a
unknown <andrec230@gmail.com>
2020-04-13 08:30:53 -0500

    se agrega validaciones de usuario

--- server/server.js
+++ server/server.js
@@ -2,7 +2,9 @@ require('./config/config') //ejecuta primero la configuración
 
 const express = require('express')
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose');
 const app = express()
+
 
 
 // parse application/x-www-form-urlencoded
@@ -11,35 +13,14 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
-app.get('/usuario', function (req, res) {
-  res.json('get usuario')
-})
+// importamos y usamos las rutas de usuario
+app.use(require('./routes/usuario'))
 
-app.post('/usuario', function (req, res) {
 
-    let body = req.body
-
-    if(!body.nombre) {
-        res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre es requerido'
-        })
-    } else {
-        res.json({body})
-    }
-
-})
-
-app.put('/usuario/:id', function (req, res) {
-    let userId = req.params.id
-    res.json({
-        id: userId
-    })
-})
-
-app.delete('/usuario', function (req, res) {
-    res.json('delete usuario')
-})
+mongoose.connect('mongodb://localhost:27017/cafe', (err, res) => {
+    if (err) throw err
+    console.log('Base de datos online...')  
+});
 
 app.listen(process.env.PORT, () => {
     console.log('Escuchando puerto: ', process.env.PORT)
