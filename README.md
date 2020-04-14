c982eddbcb7c05eea54bdb884d901aa400dee213
unknown <andrec230@gmail.com>
2020-04-13 19:07:18 -0500

    Editing README.md

---
+++ README.md
@@ -0,0 +1,3 @@
+# Node REST server connecting to mongo cloud service
+- Don't forget execute
+```npm install```
--- server/config/config.js
+++ server/config/config.js
@@ -10,7 +10,7 @@ let urlDB
 if(process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe'
 } else {
-    urlDB = 'mongodb+srv://andrec230:alCveqNxjPmb40qK@cluster0-4rrfs.mongodb.net/cafe'
+    urlDB = 'Here your url colud mongo connection'
 }
 
 process.env.URLDB = urlDB
